3c40ac780aa3e2154568b4fe441dd1164c05333b
xiongcaho <805833031@qq.com>
2021-09-08 23:15:57 +0800

    新增四则运算

---
+++ app/Commands/Arithmetic.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Commands;
+
+use Fintara\Tools\Calculator\Calculator;
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+use NXP\MathExecutor;
+
+class Arithmetic extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'size';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Calculator for arithmetic expressions';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $input = $this->ask('Please input expressions?');
+
+        $executor = new MathExecutor();
+        $result = $executor->execute($input);
+        $this->info($input . " = " . $result);
+
+//        echo $executor->execute('1 + 2 * (2 - (4+10))^2 + sin(10)').PHP_EOL;
+//        echo $executor->execute('1 + 2 * (2 - (4+10))').PHP_EOL;
+    }
+
+    /**
+     * Define the command's schedule.
+     *
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
+     * @return void
+     */
+    public function schedule(Schedule $schedule): void
+    {
+        // $schedule->command(static::class)->everyMinute();
+    }
+}
